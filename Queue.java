f645214d12e92916302baad7ef8d4281d0875745
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-17 11:27:15 -0700

    size and check is the queue is chempty

--- Queue.java
+++ Queue.java
@@ -1,6 +1,6 @@
 package exercise;
 
-public class Queue {
+public class Queue<Item> implements Iterable<Item> {
     private static final int INIT_CAPACITY = 9;
     
     // queue size
@@ -10,5 +10,23 @@ public class Queue {
     // index of the first item on the queue 
     private int first;
     // index of the ast eement on the quueue 
-    private int last;    
+    private int last; 
+    
+    
+    public Queue() {
+        q = (Item[]) new Object[INIT_CAPACITY];
+        n = 0;
+        first = 0;
+        last = 0;
+    }
+
+    // check for an empty queue size
+    public boolean isEmpty() {
+        return n == 0;
+    } 
+
+    // number of items on the queuue
+    public int size() {
+        return n;
+    }
 }
