380c7febe727dc2a031f3576ce41ceff23ad8951
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-17 11:17:39 -0700

    working on queuue resizing

---
+++ Queue.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class Queue {
+    private static final int INIT_CAPACITY = 9;
+    
+    // queue size
+    private Item[] q;
+    // number of element on queue
+    private int n;
+    // index of the first item on the queue 
+    private int first;
+    // index of the ast eement on the quueue 
+    private int last;    
+}
