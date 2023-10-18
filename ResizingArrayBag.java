54e2e4b85ae5e85c1dbc802e52a8811b11b900b9
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-18 09:33:39 -0700

    add function

--- ResizingArrayBag.java
+++ ResizingArrayBag.java
@@ -1,5 +1,35 @@
 package exercise;
 
-public class ResizingArrayBag<Item> im {
-    
+public class ResizingArrayBag<Item> implements Iterable<Item> {
+    private static final int INIT_CAPACITY = 8;
+
+    private Item[] a;
+    private int n;
+
+    public ResizingArrayBag() {
+        a = (Item[]) new Object[INIT_CAPACITY];
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++)
+         copy[i] = a[i];
+        a = copy;
+    }
+
+    public void add(Item item) {
+        if (n == a.length )
+        resize(2* a.length); 
+        a[n++] = item;
+    }
 }
