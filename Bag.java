020adc3bf0879247518861eb6dba92e17336857a
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-18 08:11:05 -0700

    done

---
+++ Bag.java
@@ -0,0 +1,78 @@
+package exercise;
+
+import java.util.NoSuchElementException;
+
+import org.w3c.dom.Node;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Bag<Item> implements Iterable<Item> {
+    private int n;
+    private Node<Item> first;
+    
+    
+    // LinkedIist heper
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+     }
+    
+
+    public Bag() {
+        first = null;
+        n = 0;
+    } 
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(Item item) {
+        Node<item> oldfirst = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    } 
+
+     public Iterator<Item> iterator()  {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public LinkedIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  {
+            return current != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bag<String> bag = new Bag<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+
+        StdOut.println("size of bag = " + bag.size());
+        for (String s : bag) {
+            StdOut.println(s);
+        }
+    }
+}
