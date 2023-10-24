6556362b9fa4a39c680eec586a7f097a87d22018
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-23 17:14:48 -0700

    sti on perc

--- WeightedQuickUnionUF.java
+++ WeightedQuickUnionUF.java
@@ -73,3 +73,117 @@ public class WeightedQuickUnionUF {
         }
     }
 }
+
+/////
+public class Percolation {
+    private int[] parent;
+    private int[] size;
+    private boolean[] openSites;
+    private int gridSize;
+    private int virtualTop;
+    private int virtualBottom;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Grid size must be greater than 0");
+        }
+        gridSize = n;
+        int totalSites = n * n;
+        parent = new int[totalSites];
+        size = new int[totalSites];
+        openSites = new boolean[totalSites];
+        virtualTop = totalSites;
+        virtualBottom = totalSites + 1;
+        
+        // Initialize parent and size arrays
+        for (int i = 0; i < totalSites; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public void open(int row, int col) {
+        int index = getIndex(row, col);
+        openSites[index] = true;
+
+        // Connect to neighboring open sites
+        connectToNeighbors(row, col);
+    }
+
+    public boolean isOpen(int row, int col) {
+        int index = getIndex(row, col);
+        return openSites[index];
+    }
+
+    public boolean isFull(int row, int col) {
+        int index = getIndex(row, col);
+        return isOpen(row, col) && isConnected(index, virtualTop);
+    }
+
+    public boolean percolates() {
+        return isConnected(virtualTop, virtualBottom);
+    }
+
+    private int getIndex(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new IllegalArgumentException("Invalid row or column");
+        }
+        return (row - 1) * gridSize + (col - 1);
+    }
+
+    private boolean isValid(int row, int col) {
+        return row >= 1 && row <= gridSize && col >= 1 && col <= gridSize;
+    }
+
+    private void connectToNeighbors(int row, int col) {
+        int[][] neighbors = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+
+        for (int[] neighbor : neighbors) {
+            int newRow = row + neighbor[0];
+            int newCol = col + neighbor[1];
+
+            if (isValid(newRow, newCol) && isOpen(newRow, newCol)) {
+                int index = getIndex(row, col);
+                int neighborIndex = getIndex(newRow, newCol);
+                union(index, neighborIndex);
+            }
+        }
+    }
+
+    private void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ) {
+            return;
+        }
+
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+
+    private int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]]; // Path compression
+            p = parent[p];
+        }
+        return p;
+    }
+
+    private boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // Change this to your desired grid size.
+        Percolation percolation = new Percolation(n);
+
+        // You can now open sites, check if they are open, check if they are full, and check if the system percolates.
+        // The implementation of these methods is provided above using the Union-Find approach.
+    }
+}
--- exercise/percolation/Percolation.java
+++ exercise/percolation/Percolation.java
@@ -4,45 +4,120 @@ public class Percolation {
     // creates n-by-n grid, with all sites initially blocked
     private boolean[][] grid;
     private int gridsize;
+    private boolean[] openSites;
+    private int[] parent;
+    private int[] size;
+    private int virtualTop;
+    private int virtualBottom; 
 
     public Percolation(int n) {
         if (n < 0) throw new IllegalArgumentException(" Grid size must be greater than n");
 
         gridsize = n;
-        grid = new boolean[n][n]; 
+        grid = new boolean[n][n];
+        int totalSites = n * n;
+        parent = new int[totalSites];
+        size = new int[totalSites];
+        openSites = new boolean[totalSites];
+        virtualBottom = totalSites - 1;
+        virtualTop = totalSites;
+        // initiaizw the parent and size arrrays
+        for (int i = 0; i < totalSites; i++) {
+            parent[i] = i;
+            size[i] = i;
+        } 
     }
 
-    public boolean isvalid(int row, int col) {
-        return row >= 1 && row <= gridsize && col >= 1 && col <= gridsize; 
-    } 
+    
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (!isvalid(row, col)) {
-            throw new IllegalArgumentException("invaid row or column");
-        }
-        grid[row - 1][col - 1] = true;
+        int index = getIndex(row, col);
+        openSites[index] = true;
+        
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        open(row, col);
-        return grid[row  -1][col - 1];
+        int index = getIndex(row, col);
+        return openSites[index];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         if ( !isvalid(row, col)) {
-            throw new IllegalAccessException("Grid size invalid")
+            throw new IllegalAccessException("Grid size invalid");
         }
-        w
+        int index = getIndex(row, col);
+        return isOpen(row, col) && isConnected(index, virtualTop);
     }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (int i = 0; i <  )
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() {
+        return isConnected(virtualTop, virtualBottom);
+    }
+
+
+    private boolean isvalid(int row, int col) {
+        return row >= 1 && row <= gridsize && col >= 1 && col <= gridsize; 
+    } 
+
+    private int getIndex(int row, int col) {
+        if (!isvalid(row, col)) throw new IllegalArgumentException("Invalid row or column");
+
+        return (row - 1) * gridsize + (col - 1);
+    }
+
+    private void ConnectedNeighbor(int row, int col) {
+        int[][] neighbors = { {-1, 0}, {1, 0}, {0, -1} , { 0, 1}};
+        
+        for (int[] neighbor : neighbors) {
+            int newRow = row + neighbor[0];
+            int newCol = col + neighbor[1];
+
+            if (isvalid(newRow, newCol) && isOpen(newRow, newCol)) {
+                int index = getIndex(row, col);
+                int neighborIndex = getIndex(newRow, newCol)
+                union(index, neighborIndex);
+            }
+        }
+    }
+
+    private void union(int p, int q)  {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ) {
+            return;
+        }
+
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] =rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+
+    private int find(int p) {
+        while ( p != parent[p] ){
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+    }
+
+    private boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
 
     // test client (optional)
-    public static void main(String[] args) 
+    public static void main(String[] args) {
+
+    }
 }
