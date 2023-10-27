7c991a52eb16ea34c0ed847ca7173289e844a575
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-27 09:26:47 -0700

    done with i t

--- BitonicArray.java
+++ BitonicArray.java
@@ -2,7 +2,47 @@ package exercise;
 
 public class BitonicArray {
     
-    
+    public static boolean BitonicArray(int[] nums, int traget) {
+
+        int peakIndex = findPeak(nums);
+
+        if (nums[peakIndex] == traget) return true;
+
+        // using binary search to find the target in the ascending order
+        int left = 0;
+        int right = peakIndex - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == traget ) {
+                return true;
+            } else if (nums[mid] < traget) {
+                left = mid + 1;
+            } else {
+                right = mid - 1; 
+            }
+        }
+        // binary serach on the descidng side 
+        left = peakIndex;
+        right = nums.length - 1; 
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == traget ) {
+                return true;
+            } else if (nums[mid] < traget) {
+                left = mid + 1;
+            } else {
+                right = mid - 1; 
+            }
+        } 
+
+        return false;
+        
+        
+    }
 
 
     public static int findPeak(int[] num) {
