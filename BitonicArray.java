a02b25e5c97431c2632e8df2bfe718eecea43fce
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-27 08:08:22 -0700

    working on the peak

--- BitonicArray.java
+++ BitonicArray.java
@@ -2,9 +2,23 @@ package exercise;
 
 public class BitonicArray {
     
+    
 
 
     public static int findPeak(int[] num) {
+        int right = num.length - 1;
+        int left = 0;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (num[mid] < num[mid + 1]) {
+                left = mid + 1; 
+            } else {
+                right = mid; 
+            }
+        }
 
+        return left;
     }
 }
