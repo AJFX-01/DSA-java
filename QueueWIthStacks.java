5af27d54ea3f6ec81efb04d149ab00d2618ec50f
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-31 05:18:18 -0700

    insgtantiating the calss

--- QueueWIthStacks.java
+++ QueueWIthStacks.java
@@ -1,4 +1,7 @@
-public class QueueWithStacks{
+import java.util.Stack;
 
-    p
+public class QueueWithStacks<T>{
+
+    private Stack<T> stack1 = new Stack<>();
+    private Stack<T> stack2 = new Stack<>(); 
 }
