b5460f028c1ef6a4b0582f48dda2b6c0f0259f42
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-31 16:24:27 -0700

    working on the random

--- exercise/queue/RandomizedQueue.java
+++ exercise/queue/RandomizedQueue.java
@@ -1,10 +1,18 @@
 package exercise.queue;
 
+import java.util.Random;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private Item[] item;
+    private int size;
+    private Random random; 
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-        
+
+        item = (Item)new Item[2];
+
     }
 
     // is the randomized queue empty?
