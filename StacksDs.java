e32f99ddd5f64410d7a9593ec91a23a2f39cc97e
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-31 06:25:49 -0700

    almost done

--- StacksDs.java
+++ StacksDs.java
@@ -3,5 +3,28 @@ import java.util.Stack;
 public class StacksDs {
     
     private Stack<Double> mainStack = new Stack<>();
-    private Stack<Double> mainStack = new Stack<>();
+    private Stack<Double> maxStack = new Stack<>();
+
+    public void push(double value) {
+        mainStack.push(value);
+
+        if (maxStack.isEmpty() || value >= maxStack.peek()) {
+            maxStack.push(value);
+        }
+    }
+    
+    public Double pop() {
+        if (mainStack.isEmpty()) {
+            return null;
+        }
+
+        double poppedValue = mainStack.pop();
+
+        if (poppedValue == maxStack.peek()) {
+            maxStack.pop();
+        }
+
+        return poppedValue;
+    } 
+
 }
