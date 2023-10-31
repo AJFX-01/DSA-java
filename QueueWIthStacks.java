de87419575d08b01c97545fa3c2f5ffa69348372
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-31 05:34:10 -0700

    done

--- QueueWIthStacks.java
+++ QueueWIthStacks.java
@@ -1,7 +1,33 @@
 import java.util.Stack;
 
-public class QueueWithStacks<T>{
+public class QueueWIthStacks<T>{
 
     private Stack<T> stack1 = new Stack<>();
     private Stack<T> stack2 = new Stack<>(); 
+
+
+    public void enqueue(T item) {
+        stack1.push(item);
+    }
+
+    public T dequeue() {
+        if(stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        if (stack2.isEmpty()) {
+            return null;
+        }
+
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueWIthStacks<Integer> queue = new QueueWIthStacks<>();
+
+        queue.enqueue(1);
+        d
+    }
 }
