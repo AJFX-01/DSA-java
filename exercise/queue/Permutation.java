1a8c393c71869eb1670e9e13fd212bd492d6b558
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-01 10:13:40 -0700

    done

--- exercise/queue/Permutation.java
+++ exercise/queue/Permutation.java
@@ -4,17 +4,17 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         int k = Integer.parseInt(args[0]);
 
-        RandomizedQueue<String> rands = new RandomizedQueue<>();
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
 
         while (!StdIn.isEmpty()) {
-            rands.enqueue(StdIn.readString());            
+            queue.enqueue(StdIn.readString());            
         }
 
         for (int i = 0; i < k; i++) {
-            StdOutprintIn(rands.dequeue());
+            System.out.println(queue.dequeue());  
         }
     }
 }
