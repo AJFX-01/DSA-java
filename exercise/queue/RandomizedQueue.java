203e4926781f6567e8b89cb9fd3aeeb96f095ca6
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-31 17:00:11 -0700

    competeing the sizing the function

--- exercise/queue/RandomizedQueue.java
+++ exercise/queue/RandomizedQueue.java
@@ -11,27 +11,49 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     // construct an empty randomized queue
     public RandomizedQueue() {
 
-        item = (Item)new Item[2];
-
+        item = (Item[]) new Object[2];
+        size = 0;
+        random = new Random();
     }
 
     // is the randomized queue empty?
-    public boolean isEmpty()
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     // return the number of items on the randomized queue
-    public int size()
+    public int size() {
+        return size; 
+    }
 
     // add the item
-    public void enqueue(Item item)
+    public void enqueue(Item item) {
+        if(item == null) { 
+            throw new IllegalAccessException("cannot add a nu vaue");
+        } 
+
+        if ()
+    }
 
     // remove and return a random item
     public Item dequeue()
+
+
 
     // return a random item (but do not remove it)
     public Item sample()
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator()
+
+
+    private void resize(int capacity) {
+        Item[] copyItems = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            item[i] = copyItems[i];
+        }
+        item = copyItems;
+    }
 
     // unit testing (required)
     public static void main(String[] args)
